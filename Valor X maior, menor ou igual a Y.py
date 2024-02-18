39684109dd24cf7b7c86c6ef7bf4e3d480ab0fc1
Misael de Castro Bezerra <157545220+Misaeldecastro@users.noreply.github.com>
2024-02-17 23:48:24 -0300

    Add files via upload

---
+++ Valor X maior, menor ou igual a Y.py
@@ -0,0 +1,11 @@
+#Exemplo if alinhado
+x = int(input("Digite x: "))
+y = int(input("Digite y: "))
+
+if x==y:
+    print("x e y são iguais")
+else:
+    if x>y:
+        print("X é maior que Y")
+    else:
+        print("X é menor que Y")
